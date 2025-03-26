f086c45e4d5878c7c779223e312b8f6abc31fce4
3208247128 <3208247128@qq.com>
2025-03-26 23:46:13 +0800

    2025/3/26

---
+++ opencvstudy/SECTION2/convert.py
@@ -0,0 +1,11 @@
+import cv2
+
+image = cv2.imread("opencvstudy\SECTION2\image_01.png")
+cv2.imshow("Original",image)
+
+#        cv2.cvtColor(image, color)
+output = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+cv2.imshow("Gray",output)
+
+
+cv2.waitKey(0)
